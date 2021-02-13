5bf2d4da6798e583547ee4550f6403d61e0c30ab
manalmosatfa11 <manal.elnaidany95@gmaill.com>
2021-02-13 17:14:53 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/SondosAwad/blogSystem""
    This reverts commit 66ed8ea0178f77b29989a741250591f65887b089.

--- src/app/_models/blog.ts
+++ src/app/_models/blog.ts
@@ -1,10 +1,11 @@
 export class Blog {
-    constructor(public title: string, 
-        public content: string,
-        public author: number,
-        public postDate: Date,
-        public _id:number,
-        public imgURL:string,
-        public tags:string[]
+    constructor(public title?: string, 
+        public content?: string,
+        public author?: number,
+        public postDate?: Date,
+        public _id?:number,
+        public imgURL?:string,
+        public tags?:string[],
+        public comments?: number[]
         ) { }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './_services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blogSystem';
+
+  constructor(public userService: UserService, public router:Router) { }
+
+  loggedIn(){
+    return !!localStorage.getItem('token');
+  }
+
+  logout() {
+    this.userService.logout().subscribe(
+      user => {
+        localStorage.clear();
+        this.router.navigate(['/users/login']);
+      }
+    )
+  }
 }
